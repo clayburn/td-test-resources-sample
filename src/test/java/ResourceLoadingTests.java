fcfa2d7829a8686fa6207725140330f73c2c2c62
Clay Johnson <cjohnson@gradle.com>
2023-11-18 07:36:40 -0600

    Update sample to load nested test resource

--- src/test/java/ResourceLoadingTests.java
+++ src/test/java/ResourceLoadingTests.java
@@ -1,34 +1,46 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class ResourceLoadingTests {
 
-    private static final String EXPECTED = "foo";
-
-    @Test
-    void srcTestResourcesAddTest() throws IOException {
-        String fileContents = Files.readString(Path.of("src/test/resources/test.txt"));
-        assertEquals(EXPECTED, fileContents, "This test is expected to fail with run on a remote executor");
+    @ParameterizedTest(name = "{0} contains {1} when read from src/test/resources")
+    @MethodSource("argProvider")
+    void srcTestResourcesAddTest(String resource, String expected) throws IOException {
+        String fileContents = Files.readString(Path.of("src/test/resources/" + resource));
+        assertEquals(expected, fileContents, "This test is expected to fail with run on a remote executor");
     }
 
-    @Test
-    void buildResourcesTestAddTest() throws IOException {
-        String fileContents = Files.readString(Path.of("build/resources/test/test.txt"));
-        assertEquals(EXPECTED, fileContents);
+    @ParameterizedTest(name = "{0} contains {1} when read from build/resources/test")
+    @MethodSource("argProvider")
+    void buildResourcesTestAddTest(String resource, String expected) throws IOException {
+        String fileContents = Files.readString(Path.of("build/resources/test/" + resource));
+        assertEquals(expected, fileContents);
     }
 
-    @Test
-    void classpathResourceLoadingAddTest() throws IOException, URISyntaxException {
-        Path path = Paths.get(this.getClass().getClassLoader().getResource("test.txt").toURI());
+    @ParameterizedTest(name = "{0} contains {1} when loaded via classloader")
+    @MethodSource("argProvider")
+    void classpathResourceLoadingAddTest(String resource, String expected) throws IOException, URISyntaxException {
+        Path path = Paths.get(this.getClass().getClassLoader().getResource(resource).toURI());
         String fileContents = Files.readString(path);
-        assertEquals(EXPECTED, fileContents);
+        assertEquals(expected, fileContents);
+    }
+
+    static Stream<Arguments> argProvider() {
+        return Stream.of(
+                arguments("test.txt", "foo"),
+                arguments("x/y/nested.txt", "bar")
+        );
     }
 
 }
